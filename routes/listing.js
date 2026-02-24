a3c4e65714c6767cd3ffa0afb5dfa2314a98b477
Vishal Rajak <150019795+Vishalrajak70@users.noreply.github.com>
2026-02-25 00:04:49 +0530

    Fix import path for wrapAsync utility

--- routes/listing.js
+++ routes/listing.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/wrapAsynC.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { reviewSchema } = require("../schema.js");
 const {isLoggedIn, isOwner ,validateListing} =require("../middleware.js");
